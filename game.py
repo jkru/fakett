0730a7862a9875f3ac8f4ce1519c4b49eb78f6d8
Hackbright Student <info@hackbrightacademy.com>
2014-10-08 17:00:11 -0700

    back to step 12

--- game.py
+++ game.py
@@ -9,13 +9,81 @@ GAME_BOARD = None
 DEBUG = False
 ######################
 
-GAME_WIDTH = 3
-GAME_HEIGHT = 3
+GAME_WIDTH = 5
+GAME_HEIGHT = 5
 
 #### Put class definitions here ####
-pass
+class Rock (GameElement):
+    IMAGE = "Rock"
+    SOLID = True
+
+class Character(GameElement):
+    IMAGE = "Girl"
+
+    def next_pos(self, direction):
+        if direction == "up":
+            return (self.x, self.y-1)
+        elif direction == "down":
+            return (self.x, self.y+1)
+        elif direction == "left":
+            return (self.x-1, self.y)
+        elif direction == "right":
+            return (self.x+1, self.y)
+        return None
+
+    def keyboard_handler(self, symbol, modifier):
+
+        direction = None
+
+        if symbol == key.UP:
+            direction = "up"
+        elif symbol == key.DOWN:
+            direction = "down"
+        elif symbol == key.LEFT:
+            direction = "left"
+        elif symbol == key.RIGHT:
+            direction = "right"
+        
+        self.board.draw_msg("[%s] moves %s" % (self.IMAGE, direction))
+
+        if direction:
+            next_location = self.next_pos(direction)
+
+            if next_location:
+                next_x = next_location[0]
+                next_y = next_location[1]
+
+                existing_el = self.board.get_el(next_x, next_y)
+
+                if existing_el and existing_el.SOLID:
+                    self.board.draw_msg("There is something in my way!")
+
+                elif existing_el is None or not existing_el.SOLID:
+                    self.board.del_el(self.x, self.y)
+                    self.board.set_el(next_x, next_y, self)
+
+
+
 ####   End class definitions    ####
 
 def initialize():
     """Put game initialization code here"""
-    pass
+    rock_positions = [
+        (2, 1),
+        (1, 2),
+        (3, 2),
+        (2, 3)
+        ]
+    rocks = []
+    for pos in rock_positions:
+        rock = Rock()
+        GAME_BOARD.register(rock)
+        GAME_BOARD.set_el(pos[0], pos[1], rock)
+        rocks.append(rock)
+
+    rocks[-1].SOLID = False
+
+    player = Character()
+    GAME_BOARD.register(player)
+    GAME_BOARD.set_el(2, 2, player)
+    print player
